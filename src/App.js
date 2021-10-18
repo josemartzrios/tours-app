e73458b04dcba2375ab5077f614ebf1b28c520c1
Jose Marti Rios <josemartzrios14@gmail.com>
2021-10-18 13:40:56 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// Components
+import Loading from './components/Loading';
+import Tour from './components/Tour';
+import Tours from './components/Tours';
+
+
+// API
+const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // Las constantes que guardan mi información
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+  // Petición fetch a mi API
+  const fetchTours = async() => {
+    setLoading(true);
+    
+  
+
+  // Por si surge algún error
+  try{
+    const response = await fetch(url);
+    const tours = await response.json();
+    setLoading(false);
+    setTours(tours)
+  }catch(error){
+    setLoading(false)
+    console.log(error);
+  }
+
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // Si loading es true que se llame al loading component
+  if(loading){
+    return (
+      <main>
+        <Loading/>
+      </main>
+    );
+  }
+  // Para refrescar la pag y mostrar todos los proyectos de nuevo
+  if(tours.length === 0){
+    return <main>
+      <div className='title'>
+        <h2>No hay tours restantes</h2>
+        <button className='btn' onClick={fetchTours}>
+          Refrescar
+        </button>
+      </div>
+    </main>
+  }
+  // En caso de que loading sea false que se llame al tours component
+  return <main>
+    <Tours tours={tours} removeTour={removeTour}/>
+  </main>
 }
 
 export default App;
---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Loading = () => {
+    return (
+        <div className="loading">
+            <h1>Cargando...</h1>
+        </div>
+    );
+}
+
+export default Loading;
